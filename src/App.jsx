9f98a346dad72513445c8cba416b98c0dc7e0d5a
Giabao11082005 <hoanggiabao110820055@gmail.com>
2025-08-07 22:59:07 +0700

    add dark light mode system

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,56 @@ import Button from "@mui/material/Button";
 import { useColorScheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlined from "@mui/icons-material/DarkModeOutLined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    console.log(selectedMode);
+    setMode(selectedMode);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Age</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <LightModeIcon fontSize="small"></LightModeIcon>
+            Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <DarkModeOutlined fontSize="small"></DarkModeOutlined>
+            Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <SettingsBrightnessIcon fontSize="small"></SettingsBrightnessIcon>
+            System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -30,9 +80,11 @@ function App() {
         alignItems: "center",
         justifyContent: "center",
         gap: 2,
+        p: 2,
       }}
     >
-      <Typography variant="h4">Hello Theme!</Typography>
+      <Typography variant="h4">Theme Mode Demo</Typography>
+      <ModeSelect />
       <ModeToggle />
     </Box>
   );
