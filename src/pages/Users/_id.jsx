f20babdbbbe099d9d998358efcb2805c91407e30
Giabao11082005 <hoanggiabao110820055@gmail.com>
2025-08-08 23:40:49 +0700

    refactor project structure and fix vite

---
+++ src/pages/Users/_id.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+return (
+  <Container disableGutters maxWidth="false" sx={{ height: "100vh" }}>
+    <Box
+      sx={{
+        backgroundColor: "primary.light",
+        width: "100%",
+        height: (theme) => theme.trello.appBarHeight,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <ModeSelect />
+    </Box>
+    <Box
+      sx={{
+        backgroundColor: "primary.dark",
+        width: "100%",
+        height: (theme) => theme.trello.boardBarHeight,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      Board Bar
+    </Box>
+    <Box
+      sx={{
+        backgroundColor: "primary.main",
+        width: "100%",
+        height: (theme) =>
+          `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      Board Content
+    </Box>
+    <></>
+  </Container>
+);
+
+export default Board;
